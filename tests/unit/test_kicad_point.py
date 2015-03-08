62ed9906f283f22a05fecf4586d4133c9d4bd4ca
Miguel Angel Ajo <miguelangel@ajo.es>
2015-03-08 15:48:47 +0100

    Test Size substraction and addition.
    wxSize didn't seem to have + or - implemented, so it's
    natively implemented in python now.

--- tests/unit/test_kicad_point.py
+++ tests/unit/test_kicad_point.py
@@ -15,3 +15,9 @@ class TestKicadPoint(unittest.TestCase):
         p = Point(1, 1)
         p_rotated = p.rotated(180, around=(1, 1))
         self.assertEqual(p_rotated.mm, (1.0, 1.0))
+
+    def test_equal(self):
+        p1 = Point(2, 2)
+        p2 = Point(2, 2)
+        self.assertTrue(p1 == p2)
+        self.assertTrue(p1 == (2, 2))
